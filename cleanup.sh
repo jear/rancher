af3327ecf7aa1975b16d6aa91c05b13594f623d6
jear <jear@users.noreply.github.com>
2022-08-16 11:26:54 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,19 @@
+# https://docs.rke2.io/install/linux_uninstall/
+# https://rancher.com/docs/rancher/v2.5/en/cluster-admin/cleaning-cluster-nodes/
+
+
+/usr/local/bin/rke2-uninstall.sh
+
+
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker volume rm -f $(docker volume ls)
+
 sudo rm -rf /run/secrets/kubernetes.io
 sudo rm -rf /var/lib/etcd
 sudo rm -rf /var/lib/kubelet
 sudo rm -rf /var/lib/rancher
 sudo rm -rf /etc/kubernetes
 sudo rm -rf /opt/rke
+
 sudo systemctl restart docker
